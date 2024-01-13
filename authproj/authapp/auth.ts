6bb2fa4d9af558d707faa8ec9ce40fb50fac9af8
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2024-01-12 19:03:53 -0500

    Routing in progress

--- authproj/authapp/auth.ts
+++ authproj/authapp/auth.ts
@@ -1,12 +1,14 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
+
+import { db } from "./lib/db";
 import authConfig from "./auth.config";
 
-const prisma = new PrismaClient();
-
-export const { handlers, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
 });
--- authproj/authapp/middleware.ts
+++ authproj/authapp/middleware.ts
@@ -1,14 +1,32 @@
-// not next-ath specifc rather next js
-import { auth } from "./auth";
-import authConfig from "./auth.config";
 import NextAuth from "next-auth";
-export const { auth: middleware } = NextAuth(authConfig);
+import authConfig from "./auth.config";
+import {
+  DEFAULT_LOIGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from "./routes";
+const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
+  const { nextUrl } = req;
+  // checking if logged in
   const isLoggedIn = !!req.auth;
-  // req.auth
-  console.log("ROUTE", req.nextUrl.pathname);
-  console.log("Logged in", isLoggedIn);
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return null;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOIGIN_REDIRECT, nextUrl));
+    }
+    return null;
+  }
 });
 
 // wont be used to check public or pvt . rather to invoke the middleware fucntion
